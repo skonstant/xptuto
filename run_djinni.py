23de9ad1fc8fcf252e17bfac6eb83f1580fe7c2c
stephane <stephane@voyageonline.co.uk>
2020-05-02 22:54:02 +0200

    initial

--- run_djinni.py
+++ run_djinni.py
@@ -11,18 +11,12 @@ djinni_dir = base_dir + "/djinni"
 
 api_in = djinni_dir + "/main.djinni"
 api_temp_out = base_dir + "/scripts/djinni-output-api-temp"
-api_java_package = "app.common"
+api_java_package = "org.example.xptuto"
 api_gen_stamp = api_temp_out + "/gen.api.stamp"
 api_cpp_out = base_dir + "/src/gen"
-api_cpp_namespace = "ac"
-api_java_out = base_dir + "/android/app-common-aar/appcommon/src/main/gen_java/app/common"
-api_jni_out = base_dir + "/android/app-common-aar/appcommon/src/main/cpp"
-api_objc_out = base_dir + "/ios/gen"
-
-proxygen_dir = base_dir + "/tools/proxygen"
-proxygen_objc_in = proxygen_dir + "/data/delegates"
-proxygen_proxies_out = proxygen_dir + "/data/delegate-proxies"
-api_proxies_out = base_dir + "/ios/proxy"
+api_cpp_namespace = "xptuto"
+api_java_out = base_dir + "/android/app/src/main/gen_java/org/example/xptuto"
+api_jni_out = base_dir + "/android/app/src/main/cpp/gen"
 
 # Clean command-line argument
 if len(sys.argv) > 1:
@@ -31,7 +25,7 @@ if len(sys.argv) > 1:
         print ("Unexpected command: " + command)
         sys.exit(1)
     else:
-        for folder in [api_temp_out, api_cpp_out, api_java_out, api_jni_out, api_objc_out, api_proxies_out]:
+        for folder in [api_temp_out, api_cpp_out, api_java_out, api_jni_out]:
             print ("Deleting " + folder)
             shutil.rmtree(folder, ignore_errors=True)
         sys.exit(0)
@@ -43,16 +37,10 @@ os.system(base_dir + "/deps/djinni/src/build")
 djinni_cmd = ("{base_dir}/deps/djinni/src/run-assume-built "
               "--cpp-out {temp_out}/cpp "
               "--cpp-namespace {cpp_namespace} "
-              "--ident-cpp-enum-type foo_bar "
               "--java-out {temp_out}/java "
               "--java-package {java_package} "
-              "--ident-java-field mFooBar "
               "--jni-out {temp_out}/jni "
-              "--ident-jni-class NativeFooBar "
-              "--ident-jni-file native_foo_bar "
-
               "--idl {in_file} ")
-
 
 def mirror(prefix, from_path, to_path):
     if not os.path.exists(to_path):
@@ -70,13 +58,6 @@ if os.path.exists(api_in):
     mirror("cpp", api_temp_out + "/cpp", api_cpp_out)
     mirror("java", api_temp_out + "/java", api_java_out)
     mirror("jni", api_temp_out + "/jni", api_jni_out)
-    mirror("objc", api_temp_out + "/objc", api_objc_out)
-
-    mirror("proxygen_in", api_temp_out + "/objc", proxygen_objc_in)
-    proxygen_cmd = proxygen_dir + "/proxygen.py"
-    os.system(proxygen_cmd)
-    mirror("proxygen_out", proxygen_proxies_out, api_proxies_out)
-
     os.system("date > " + api_gen_stamp)
 else:
     print ("Error:" + api_in + "not found!")
