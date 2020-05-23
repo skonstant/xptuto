7e17867535c738083cad22a07feb7ee2b70056ce
Stephane Konstantaropoulos <stephane@voyageonline.co.uk>
2020-05-23 18:07:30 +0200

    visible app

---
+++ android/app/src/main/java/org/example/xptuto/ListActivity.java
@@ -0,0 +1,97 @@
+package org.example.xptuto;
+
+import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.text.DateFormat;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+public class ListActivity extends AppCompatActivity {
+
+    public static final String USER_NAME = "arg_user_name";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setAdapter(new MyAdapter(getIntent().getStringExtra(USER_NAME)));
+    }
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        public final TextView name;
+        public final TextView description;
+        public final TextView creationDate;
+
+        public MyViewHolder(View v) {
+            super(v);
+            name = v.findViewById(R.id.name);
+            description = v.findViewById(R.id.descr);
+            creationDate = v.findViewById(R.id.creation_date);
+        }
+    }
+
+    public class MyAdapter extends RecyclerView.Adapter<MyViewHolder> {
+        private final Xptuto x;
+        private List<Repo> repos;
+
+        public MyAdapter(String userName) {
+            x = Xptuto.makeInstance(new JavaHttpClient(getApplicationContext()), new AndroidThreads());
+            x.getReposForUserName(userName, new GetReposCb() {
+                @Override
+                public void onSuccess(ArrayList<Repo> r, User u) {
+                    repos = r;
+                    notifyDataSetChanged();
+                }
+
+                @Override
+                public void onError(String error) {
+                    finish();
+                }
+            });
+        }
+
+        @NotNull
+        @Override
+        public MyViewHolder onCreateViewHolder(ViewGroup parent,
+                                               int viewType) {
+            return new MyViewHolder(LayoutInflater.from(parent.getContext()).inflate(R.layout.list_item, parent, false));
+        }
+
+        @Override
+        public void onBindViewHolder(@NotNull MyViewHolder holder, int position) {
+            Repo repo = repos.get(position);
+            String str = repo.name + " (" + repo.fullName + ")";
+            if (repo.priv) {
+                str += " - private";
+            }
+            holder.name.setText(str);
+            if (!TextUtils.isEmpty(repo.descr)) {
+                holder.description.setText(repo.descr);
+            } else {
+                holder.description.setText("No description");
+            }
+
+            holder.creationDate.setText(getString(R.string.created_on, DateFormat.getDateInstance(
+                    DateFormat.LONG, Locale.getDefault()).format(repo.createdAt)));
+        }
+
+        @Override
+        public int getItemCount() {
+            return repos == null ? 0 : repos.size();
+        }
+    }
+
+}
--- android/app/src/main/java/org/example/xptuto/MainActivity.java
+++ android/app/src/main/java/org/example/xptuto/MainActivity.java
@@ -1,20 +1,44 @@
 package org.example.xptuto;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
+import android.view.View;
+import android.view.inputmethod.EditorInfo;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.widget.ContentLoadingProgressBar;
 
-import java.util.ArrayList;
+import com.bumptech.glide.Glide;
+
+import java.text.DateFormat;
+import java.util.Locale;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String TAG = "XP";
+    private static final String LOADED = "arg_loaded";
 
     // Used to load the 'native-lib' library on application startup.
     static {
         System.loadLibrary("native-lib");
     }
+
+    private ContentLoadingProgressBar progressBar;
+    private TextView notFound;
+    private View details;
+    private EditText editText;
+    private TextView userName;
+    private TextView creationDate;
+    private ImageView avatar;
 
     private Xptuto x;
 
@@ -22,47 +46,70 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         x = Xptuto.makeInstance(new JavaHttpClient(getApplicationContext()), new AndroidThreads());
-        x.getUser("aosp", new GetUserCb() {
-            @Override
-            public void onSuccess(User user) {
-                Log.d(TAG, "Got a user with name: " + user.login);
-                x.getReposForUser(user, new GetReposCb() {
+
+        progressBar = findViewById(R.id.progress);
+        notFound = findViewById(R.id.notFound);
+        details = findViewById(R.id.details);
+        editText = findViewById(R.id.user_name_input);
+        userName = findViewById(R.id.user_name);
+        creationDate = findViewById(R.id.creation_date);
+        avatar = findViewById(R.id.avatar);
+
+        editText.setOnEditorActionListener((v, actionId, event) -> {
+            if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                InputMethodManager imm = (InputMethodManager) getSystemService(Activity.INPUT_METHOD_SERVICE);
+                imm.hideSoftInputFromWindow(v.getWindowToken(), 0);
+
+                details.setVisibility(View.INVISIBLE);
+                notFound.setVisibility(View.INVISIBLE);
+                progressBar.show();
+
+                x.getUser(v.getText().toString(), new GetUserCb() {
                     @Override
-                    public void onSuccess(ArrayList<Repo> repos, User u) {
-                        for (Repo repo : repos) {
-                            Log.d(TAG, "Got a repo with name: " + repo.name);
-                        }
+                    public void onSuccess(User user) {
+                        Log.d(TAG, "Got a user with name: " + user.login);
+                        progressBar.hide();
+                        details.setVisibility(View.VISIBLE);
+                        userName.setText(user.login);
+                        creationDate.setText(DateFormat.getDateInstance(
+                                DateFormat.MEDIUM, Locale.getDefault()).format(user.createdAt));
+                        avatar.setImageDrawable(null);
+                        Glide.with(MainActivity.this)
+                                .load(user.avatarUrl).into(avatar);
                     }
 
                     @Override
                     public void onError(String error) {
                         Log.e(TAG, "Error: " + error);
+                        progressBar.hide();
+                        notFound.setVisibility(View.VISIBLE);
                     }
                 });
-            }
 
-            @Override
-            public void onError(String error) {
-                Log.e(TAG, "Error: " + error);
+                return true;
             }
+            return false;
         });
 
-        x.getReposForUserName("aosp", new GetReposCb() {
-            @Override
-            public void onSuccess(ArrayList<Repo> repos, User u) {
-                Log.d(TAG, "got user with login: " + u.login);
-                for (Repo repo : repos) {
-                    Log.d(TAG, "Got a repo with name: " + repo.name);
-                }
-            }
+        if (savedInstanceState != null && savedInstanceState.getBoolean(LOADED)) {
+            editText.post(() -> editText.onEditorAction(EditorInfo.IME_ACTION_SEARCH));
+        }
+    }
 
-            @Override
-            public void onError(String error) {
-                Log.e(TAG, "Error: " + error);
-            }
-        });
+    public void viewRepositories(View view) {
+        if (!TextUtils.isEmpty(userName.getText())) {
+            Intent intent = new Intent(this, ListActivity.class);
+            intent.putExtra(ListActivity.USER_NAME, userName.getText().toString());
+            startActivity(intent);
+        }
+    }
 
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        if (!TextUtils.isEmpty(userName.getText())) {
+            outState.putBoolean(LOADED, true);
+        }
+        super.onSaveInstanceState(outState);
     }
 }
